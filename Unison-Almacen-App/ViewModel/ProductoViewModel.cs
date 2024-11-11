91614a6dbfb6e655420690fa4585f4f4c98cfc83
Anattnas <anattnas@gmail.com>
2024-11-10 19:57:56 -0700

    Initial commit

---
+++ Unison-Almacen-App/ViewModel/ProductoViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Unison_Almacen_Core.Modelos;
+using Unison_Almacen_Core.BaseDeDatos;
+using Unison_Almacen_Core.Repositorios;
+using System.Windows;
+namespace Unison_Almacen_App.ViewModel;
+
+public partial class ProductoViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private Producto producto = new Producto();
+
+    private readonly ProductoRepositorio _productoRepositorio;
+
+    public ProductoViewModel()
+    {
+        _productoRepositorio = new ProductoRepositorio();
+        AgregarProductoCommand = new RelayCommand(AgregarProducto);
+    }
+
+    public ICommand AgregarProductoCommand { get; }
+
+    private void AgregarProducto()
+    {
+        // Validar que los campos 
+        if (string.IsNullOrWhiteSpace(Producto.Nombre) ||
+            string.IsNullOrWhiteSpace(Producto.Descripcion) ||
+            Producto.Precio <= 0)
+        {
+            MessageBox.Show("Completa todos los campos.", 
+                "Campos Vacíos", 
+                MessageBoxButton.OK, 
+                MessageBoxImage.Warning);
+            return;
+        }
+
+        // Agregar el producto a la base de datos
+        _productoRepositorio.Agregar(Producto);
+
+        // Limpiar los campos
+        Producto = new Producto();
+    }
+}
---
+++ Unison-Almacen-App/Views/ProductoView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Unison_Almacen_App.ViewModel;
+
+namespace Unison_Almacen_App.Views;
+
+public partial class ProductoView : Page
+{
+    public ProductoView(ProductoViewModel viewModel)
+    {
+        InitializeComponent();
+        
+        DataContext = viewModel;
+    }
+}
